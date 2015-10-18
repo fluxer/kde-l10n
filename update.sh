5ae2f30a92a9010c9c4b11064f78fbbf9c737321
Ivailo Monev <xakepa10@gmail.com>
2015-10-18 03:16:51 +0300

    update.sh: remove entries before adding them

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed '/macro_optional_add_subdirectory/d' -i CMakeLists.txt
 for i in *;do
     if [ -d "$i" ];then
         echo "macro_optional_add_subdirectory( $i )" >> CMakeLists.txt
