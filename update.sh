fc44b7a2c6241198c46b61e5543aaccf8152e2bb
Ivailo Monev <xakepa10@gmail.com>
2022-10-02 17:06:07 +0300

    generic: drop support for building and installing from subdirectory
    after configuration is done installation of translations for one
    language only can still be done via:
    make -C <subdir> install
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed 'kde4_optional_add_subdirectory' -i CMakeLists.txt
+sed 'add_subdirectory' -i CMakeLists.txt
 for i in *;do
     if [ -d "$i" ];then
-        echo "kde4_optional_add_subdirectory( $i )" >> CMakeLists.txt
+        echo "add_subdirectory( $i )" >> CMakeLists.txt
     fi
 done
